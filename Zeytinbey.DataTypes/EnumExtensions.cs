4a407c78138f28686e39e9c69aac1c3475f8842f
sinanilyas <sinan@sinanilyas.com>
2023-11-25 15:47:12 +0300

    Enum extension method DisplayName is renamed as GetDisplayNameOrNull. Added GetDisplayName extension method which returns empty string if display name is not specified.

--- Zeytinbey.DataTypes/EnumExtensions.cs
+++ Zeytinbey.DataTypes/EnumExtensions.cs
@@ -9,8 +9,18 @@ public static class EnumExtensions
     /// Extension method to retrieve the 'Name' value specified in the Display DataAnnotation for the given enum value.
     /// </summary>
     /// <param name="enumValue">The enum value for which to retrieve the 'Name' value.</param>
-    /// <returns>The 'Name' value specified in the Display DataAnnotation, or null if there is no Display DataAnnotation.</returns>
-    public static string? DisplayName(this Enum enumValue)
+    /// <returns>The 'Name' value specified in the Display DataAnnotation, or empty string if display name is not specified.</returns>
+    public static string GetDisplayName(this Enum enumValue)
+    {
+        return enumValue.GetDisplayNameOrNull() ?? string.Empty;
+    }
+
+    /// <summary>
+    /// Extension method to retrieve the 'Name' value specified in the Display DataAnnotation for the given enum value.
+    /// </summary>
+    /// <param name="enumValue">The enum value for which to retrieve the 'Name' value.</param>
+    /// <returns>The 'Name' value specified in the Display DataAnnotation, or null if display name is not specified.</returns>
+    public static string? GetDisplayNameOrNull(this Enum enumValue)
     {
         return enumValue.GetType()
             .GetMember(enumValue.ToString())

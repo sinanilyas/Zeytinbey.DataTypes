5aadce245208e9b11a03b218754e8809185169fa
sinanilyas <sinan@sinanilyas.com>
2024-01-07 16:46:32 +0300

    Implemented an extension method for the bool type, enabling conversion to a string with specified true and false string values.

---
+++ Zeytinbey.DataTypes/BoolExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace Zeytinbey.DataTypes;
+
+public static class BoolExtensions
+{
+	public static string ToString(this bool value, string trueValue, string falseValue)
+	{
+		return value ? trueValue : falseValue;
+	}
+}

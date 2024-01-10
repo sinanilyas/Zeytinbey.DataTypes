33a191336f0bb40b189e3f0bea8c3c83497cf0fb
sinanilyas <sinan@sinanilyas.com>
2024-01-10 20:57:11 +0300

    Added EnumHelper class with GetValues<T> and FindByValue<T> methods.

---
+++ Zeytinbey.DataTypes/EnumHelper.cs
@@ -0,0 +1,16 @@
+﻿namespace Zeytinbey.DataTypes;
+
+public static class EnumHelper
+{
+	public static IEnumerable<T> GetValues<T>() where T : struct
+	{
+		return Enum.GetValues(typeof(T)).Cast<T>();
+	}
+
+	public static T? FindByValue<T>(string? value) where T : struct
+	{
+		if (Enum.TryParse(value, out T result)) return result;
+
+		return null;
+	}
+}
